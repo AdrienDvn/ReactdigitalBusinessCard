d2a4ed4bdeed6882fd6cff89590d5750e7d39939
adriendavin <adriendavin@hotmail.fr>
2023-02-21 18:06:40 +0100

    initialize components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+// import react maybe?
+// import picture here to display them
+
+export default function Header() {
+  return (
+    <div>
+      <h5>Header photo here</h5>
+      <h5>person infos here</h5>
+
+    </div>
+  );
+}
