9b50da3de8a744515d7682efa2e2ab12e35aff98
adriendavin <adriendavin@hotmail.fr>
2023-02-21 19:25:27 +0100

    card outter shell css ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
+import './index.css';
+import '/Users/adrien/code/BobZiroll/digitalbusinesscard/src/card.css';
 
 import Card from './components/Card.js';
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import React from 'react';
-//import the card.css HERE;
 import Header from '/Users/adrien/code/BobZiroll/digitalbusinesscard/src/components/Header.js';
 import Contact from '/Users/adrien/code/BobZiroll/digitalbusinesscard/src/components/Contact.js';
 import Main from '/Users/adrien/code/BobZiroll/digitalbusinesscard/src/components/Main.js';
 import Footer from '/Users/adrien/code/BobZiroll/digitalbusinesscard/src/components/Footer.js';
+
+//import the card.css HERE;
 
 export default function Card() {
   return (
